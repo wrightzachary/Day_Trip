8fd867aff0da78ff08c5ccae125ded81ec6796c4
wrightzachary <taylorzw96@gmail.com>
2021-05-28 14:33:49 -0500

    random generator not in function

---
+++ DayTrip.js
@@ -0,0 +1,19 @@
+// random destination
+let destination = ['Austin', 'Dallas', 'San Antonio', 'Houston'];    
+let randomDestination = destination[Math.floor(Math.random() * 4)];
+console.log(randomDestination);
+
+// random restaurant
+let restaurant = ['Pluckers', 'Papadeoux', 'Cheddars', 'Food Trucks'];
+let randomRestaurant = restaurant[Math.floor(Math.random() * 4)];
+console.log(randomRestaurant);
+
+// random transportation
+let transportation = ['Car', 'Plane', 'Carpool With Friends', 'You choose'];
+let randomTransportation = transportation[Math.floor(Math.random() * 4)];
+console.log(randomTransportation);
+
+// random entertainment
+let entertainment = ['Bowling', 'GoKarts', 'Six Flags', 'Top Golf'];
+let randomEntertainment = entertainment[Math.floor(Math.random() * 4)];
+console.log(randomEntertainment);
